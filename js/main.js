7a1aaac0dfbd81018e43aa9c8ae080e86a8b690f
Anna Toll <anna.toll@protonmail.com>
2021-12-02 00:59:37 +0100

    Added portfolio section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+let darkBackgroundDiv = document.getElementById('darken-background-fullwidth');
+let nav = document.querySelector('header');
+
+nav.addEventListener('click', darkenBackground);
+
+function darkenBackground() {
+    if (darkBackgroundDiv.style.display == 'block') {
+        $("#darken-background-fullwidth").fadeOut(500);   
+    } else {
+        $("#darken-background-fullwidth").fadeIn(500); 
+    }
+}
+
 function openCloseSidebar() {
     let className = document.body.getAttribute("class");
     if(className == "menyoppen") {
