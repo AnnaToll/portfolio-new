373f222a72382be752909ba454e1f257afc7c3cf
Anna Toll <anna.toll@protonmail.com>
2021-12-02 16:09:18 +0100

    Added merit popup

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 let darkBackgroundDiv = document.getElementById('darken-background-fullwidth');
 let nav = document.querySelector('header');
+let meritsArr = document.querySelectorAll('#container-merits > div');
+
+for (let merit of meritsArr) {
+    let hiddenElements = document.getElementById('hidden-elements-container');
+    merit.addEventListener('click', () => {
+        for (let element of hiddenElements.children) {
+            console.log(element.firstElementChild.innerText.toLowerCase().trim());
+            if (merit.children[2].firstElementChild.innerText.toLowerCase().trim() == element.firstElementChild.innerText.toLowerCase().trim()) {
+                darkBackgroundDiv.innerHTML = `
+                    <div class="merits-popup-container">
+                        ${element.innerHTML}
+                    </div>
+                `;
+                $("#darken-background-fullwidth").fadeIn(350); 
+                darkBackgroundDiv.style.display = 'grid';
+            }
+        }
+
+    })
+}
+
+darkBackgroundDiv.addEventListener('click', () => {
+    $("#darken-background-fullwidth").fadeOut(300); 
+})
+
+
 
 nav.addEventListener('click', darkenBackground);
 
